158adcb044fc75e2e693223be0e4bb8ebeb312b9
Willie Shen <cg10250207@gmail.com>
2026-08-19 18:59:23 +0800

    2026-08-19T10:59:23+00:00

--- download-models.sh
+++ download-models.sh
@@ -9,7 +9,10 @@ hf download unsloth/Qwen3.5-2B-GGUF Qwen3.5-2B-Q4_K_M.gguf
 hf download unsloth/Qwen3.5-2B-GGUF mmproj-BF16.gguf
 hf download unsloth/gemma-4-12B-it-qat-GGUF gemma-4-12B-it-qat-UD-Q4_K_XL.gguf
 hf download unsloth/gemma-4-12B-it-qat-GGUF mmproj-BF16.gguf
+hf download unsloth/gemma-4-12B-it-qat-GGUF mtp-gemma-4-12B-it.gguf
 hf download unsloth/gemma-4-E4B-it-qat-GGUF gemma-4-E4B-it-qat-UD-Q4_K_XL.gguf
 hf download unsloth/gemma-4-E4B-it-qat-GGUF mmproj-BF16.gguf
+hf download unsloth/gemma-4-E4B-it-qat-GGUF mtp-gemma-4-E4B-it.gguf
 hf download unsloth/gemma-4-E2B-it-qat-GGUF gemma-4-E2B-it-qat-UD-Q4_K_XL.gguf
 hf download unsloth/gemma-4-E2B-it-qat-GGUF mmproj-BF16.gguf
+hf download unsloth/gemma-4-E2B-it-qat-GGUF mtp-gemma-4-E2B-it.gguf
