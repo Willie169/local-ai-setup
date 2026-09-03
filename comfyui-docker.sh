940e180cc83194ffc7f90ab34ba72148ac0f894e
Willie Shen <cg10250207@gmail.com>
2026-09-03 12:44:05 +0800

    2026-09-03T04:44:05+00:00

--- comfyui-docker.sh
+++ comfyui-docker.sh
@@ -64,4 +64,7 @@ WantedBy=default.target
 EOF
 sudo systemctl daemon-reload
 sudo systemctl enable --now comfyui
-hf download stable-diffusion-v1-5/stable-diffusion-v1-5 v1-5-pruned-emaonly.safetensors --local-dir ~/ComfyUI-Docker/storage-models/checkpoints
+hf download hf://stable-diffusion-v1-5/stable-diffusion-v1-5/v1-5-pruned-emaonly.safetensors --local-dir ~/ComfyUI-Docker/storage-models/checkpoints
+hf download hf://unsloth/FLUX.2-klein-4B-GGUF/flux-2-klein-4b-Q4_K_M.gguf --local-dir ~/ComfyUI-Docker/storage-models/checkpoints
+hf download hf://city96/stable-diffusion-3.5-medium-gguf/sd3.5_medium-Q4_K_M.gguf --local-dir ~/ComfyUI-Docker/storage-models/checkpoints
+hf download hf://Serveurperso/ACE-Step-1.5-GGUF/acestep-v15-sft-Q4_K_M.gguf --local-dir ~/ComfyUI-Docker/storage-models/checkpoints
